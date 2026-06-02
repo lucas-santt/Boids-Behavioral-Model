23c6acd102ab724529f733e121d08d207b19cb41
Lucas <lucantonio.santo@gmail.com>
2026-06-02 17:44:16 -0300

    OPENGL: Added Shader and Camera logic

---
+++ OpenGL/src/classes/camera.h
@@ -0,0 +1,121 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+const float YAW         = -90.0f;
+const float PITCH       =  0.0f;
+const float SPEED       =  2.5f;
+const float SENSITIVITY =  0.1f;
+const float ZOOM        =  45.0f;
+
+class Camera 
+{
+public:
+    glm::vec3 position;
+    glm::vec3 front;
+    glm::vec3 up;
+    glm::vec3 right;
+    glm::vec3 worldUp;
+
+    float yaw;
+    float pitch;
+
+    float movementSpeed;
+    float mouseSensitivity;
+    float zoom; // Or FOV
+
+    Camera(
+        glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3 up       = glm::vec3(0.0f, 1.0f, 0.0f),
+        float yaw          = YAW,
+        float pitch        = PITCH
+    ) : front(glm::vec3(0.0f, 0.0f, -1.0f)),
+        movementSpeed(SPEED),
+        mouseSensitivity(SENSITIVITY), 
+        zoom(ZOOM)
+    {
+        this->position = position;
+        this->worldUp  = up;
+        this->yaw      = yaw;
+        this->pitch    = pitch;
+        updateCameraVectors();
+    }
+
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch)
+        : front(glm::vec3(0.0f, 1.0f, 0.0f)),
+          movementSpeed(SPEED),
+          mouseSensitivity(SENSITIVITY),
+          zoom(ZOOM)
+    {
+        this->position = glm::vec3(posX, posY, posZ);
+        this->worldUp  = glm::vec3(upX, upY, upZ);
+        this->yaw      = yaw;
+        this->pitch    = pitch;
+        updateCameraVectors();
+    }
+
+    glm::mat4 GetViewMatrix() {
+        return glm::lookAt(position, position + front, up);
+    }
+
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime) {
+        float velocity = movementSpeed * deltaTime;
+        if(direction == FORWARD)
+            position += front * velocity;
+        else if(direction == BACKWARD)
+            position -= front * velocity;
+        else if(direction == LEFT)
+            position -= right * velocity;
+        else if(direction == RIGHT)
+            position += right * velocity;
+    }
+
+    void ProcessMouseMovement(float xOffset, float yOffset, GLboolean constraintPitch = true) {
+        xOffset *= mouseSensitivity;
+        yOffset *= mouseSensitivity;
+
+        yaw   += xOffset;
+        pitch += yOffset;
+
+        if(constraintPitch) {
+            if(pitch > 89.0f)
+                pitch = 89.0f;
+            else if (pitch < -89.0f)
+                pitch = -89.0f;
+        }
+
+        updateCameraVectors();
+    }
+
+    void ProcessMouseScroll(float yOffset) {
+        zoom -= (float) yOffset;
+        if(zoom < 1.0f)
+            zoom = 1.0f;
+        else if(zoom > 45.0f)
+            zoom = 45.0f;
+    }
+
+private:
+    void updateCameraVectors() {
+        glm::vec3 newFront;
+        newFront.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+        newFront.y = sin(glm::radians(pitch));
+        newFront.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+        
+        front = glm::normalize(newFront);
+        right = glm::normalize(glm::cross(front, worldUp));
+        up    = glm::normalize(glm::cross(right, front));
+    }
+};
+
+#endif
---
+++ OpenGL/src/classes/shader.h
@@ -0,0 +1,114 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <iostream>
+#include <string>
+
+#include <fstream>
+#include <sstream>
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader
+{
+public:
+    unsigned int ID;
+
+    Shader(const char* vertexPath, const char* fragmentPath) {
+        // 1. Reading Files
+        std::string vertexCode, fragmentCode;
+        std::ifstream vShaderFile, fShaderFile;
+
+        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+        try {
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            std::stringstream vShaderStream, fShaderStream;
+
+
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+
+            vShaderFile.close();
+            fShaderFile.close();
+
+            vertexCode   = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+        } catch(std::ifstream::failure& e) {
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: " << e.what() << std::endl;
+        }
+
+        const char* vShaderCode = vertexCode.c_str();
+        const char* fShaderCode = fragmentCode.c_str();
+
+        // 2. Compiling
+        int shaderSuccess;
+        // Vertex shader
+        unsigned int vertex;
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        
+        glGetShaderiv(vertex, GL_COMPILE_STATUS, &shaderSuccess);
+        if(!shaderSuccess) {
+            char infoLog[512];
+            glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION FAILED\n" << infoLog << std::endl;
+        }
+
+        // Fragment shader
+        unsigned int fragment;
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+
+        glGetShaderiv(fragment, GL_COMPILE_STATUS, &shaderSuccess);
+        if(!shaderSuccess) {
+            char infoLog[512];
+            glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION FAILED\n" << infoLog << std::endl;
+        }
+
+        // 3. Creating shader program - Linking
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+
+        glGetProgramiv(ID, GL_LINK_STATUS, &shaderSuccess);
+        if(!shaderSuccess) {
+            char infoLog[512];
+            glGetProgramInfoLog(ID, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINK FAILED\n" << infoLog << std::endl;
+        }
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }   
+
+    void use() { 
+        glUseProgram(ID);
+    }
+
+    void setBool(const std::string &name, bool value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int) value);
+    }
+
+    void setInt(const std::string &name, int value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+    void setFloat(const std::string &name, float value) const {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+    void setMat4(const std::string &name, glm::mat4 value) const {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+    }
+};
+
+#endif
